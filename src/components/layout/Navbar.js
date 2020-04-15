3043a077824a00ecc17d2e31020e36d6be5e30b2
LiliaRibac <ribac.lilia@gmai.com>
2020-04-15 16:58:34 -0700

    tracking auth status $$ waiting for Auth when is ready

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
-import {connect} from "react-redux"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
-const Navbar =() =>{
-  return(
+const Navbar = (props) => {
+  const { auth } = props;
+  // console.log(auth)
+
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to='' className="brand-logo">GroupPlans</Link>
-        <SignedInLinks/>
-        <SignedOutLinks/>
+        <Link to="" className="brand-logo">
+          GroupPlans
+        </Link>
+        {links}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-const mapStateToProps =(state)=>{
-  console.log(state)
-  return{
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
 
-  }
-}
-
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
