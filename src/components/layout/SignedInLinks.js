7738d196d6cc1cb93a291ba0d6c7fe0c8cf6ee86
LiliaRibac <ribac.lilia@gmai.com>
2020-04-15 16:40:24 -0700

    able logging user out

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {connect} from "react-redux"
+import {signOut } from "../../store/actions/authAction"
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut} to="/">Log Out</a>
       </li>
 
       <li>
@@ -20,4 +22,10 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps =(dispatch) =>{
+return {
+  signOut: () =>{dispatch(signOut())}
+}
+}
+
+export default connect(null,mapDispatchToProps)(SignedInLinks);
