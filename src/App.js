d0809fc7dbd18ba2adef9a80b6528ed2e9d47c68
LiliaRibac <ribac.lilia@gmai.com>
2020-04-14 17:16:25 -0700

    add lists on dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import {BrowserRouter} from "react-router-dom"
-import Navbar from './components/layout/Navbar'
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-    <div className="App">
-     <Navbar/>
-    </div>
-    </BrowserRouter>
-  )
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
