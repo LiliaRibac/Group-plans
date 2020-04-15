bd96b0497b570772f6ce6c535401fd86ad0d3a57
LiliaRibac <ribac.lilia@gmai.com>
2020-04-14 21:33:42 -0700

    add Redux && reducer to the app

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,7 @@
+const initState ={}
+
+//here we will manipulate the state when we will have data
+const authReducer =(state =initState, action) =>{
+  return state
+}
+export default authReducer
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,7 @@
+const initState ={}
+
+//here we will manipulate the state when we will have data
+const projectReducer =(state =initState, action) =>{
+  return state
+}
+export default projectReducer
